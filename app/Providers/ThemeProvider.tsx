1a0206ad9f8ff0f1d283fa29116de49bc013f478
Mahesh2-3 <maheshkarna32@gmail.com>
2026-04-06 22:53:06 +0530

    Intialized the Saas project ArchiText

---
+++ app/Providers/ThemeProvider.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'next-themes'
+import { ReactNode } from 'react'
+
+interface Props {
+    children: ReactNode
+}
+
+const ThemeProviderClient = ({ children }: Props) => {
+    return (
+        <ThemeProvider defaultTheme='system' attribute="class" enableSystem>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+export default ThemeProviderClient
