f20baa8eea281ec6745f23f354d6a70be2d0157b
Mahesh2-3 <maheshkarna32@gmail.com>
2026-04-23 09:34:48 +0530

    fix: server/Implemented cors

--- server/main.js
+++ server/main.js
@@ -46,10 +46,8 @@ app.use(
 // CORS configuration
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? process.env.FRONTEND_URL
-        : "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
+
     credentials: true,
   }),
 );
